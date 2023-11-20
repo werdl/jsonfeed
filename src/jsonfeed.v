6e5fe78fb44d851e747644c78252794a6a7c78a4
werdl <116349156+werdl@users.noreply.github.com>
2023-11-20 16:15:34 +0000

    bye

--- src/jsonfeed.v
+++ src/jsonfeed.v
@@ -1,6 +1,8 @@
 module jsonfeed
 
 import time
+import json
+import encoding.xml
 
 struct Post {
 	title string @[required]
@@ -12,9 +14,12 @@ struct Post {
 
 struct Feed {
 	pub:
-	blogname string @[required]
-	htmlurl string
-	mut: posts []Post @[required]
+		jsonfeed_v string = "0.1.0" @[required]
+ 		blogname string @[required]
+		htmlurl string @[required]
+		desc string @[required]
+	mut: 
+		posts []Post @[required]
 }
 
 pub fn Post.new(title string, author string, desc string, url string) Post {
@@ -28,12 +33,41 @@ pub fn Post.new(title string, author string, desc string, url string) Post {
 pub fn (mut f Feed) push(p Post) {
 	f.posts << p
 }
-pub fn Feed.new(blogname string, htmlurl string) Feed {
+pub fn Feed.new(blogname string, htmlurl string, desc string) Feed {
 	return Feed{
 		posts: []
 		blogname: blogname
 		htmlurl: htmlurl
+		desc: desc
 	}
+}
+
+pub fn Feed.rss(s string) Feed {
+
+
+	doc := xml.XMLDocument.from_string(s) or { panic(err) }
+	
+	chann:=doc.get_elements_by_tag("channel")[0]
+
+	title:=chann.get_elements_by_tag("title")[0]
+
+	link:=chann.get_elements_by_tag("link")[0]
+
+	desc:=chann.get_elements_by_tag("description")[0]
+
+	posts:=chann.get_elements_by_tag("item")
+
+
+	mut postss:=[]Post{}
+	for node in posts {
+		postss << Post.new(node.get_elements_by_tag("title")[0], "unknown", node.get_elements_by_tag("description")[0], node.get_elements_by_tag("link")[0])
+	}
+
+
+
+	mut x:=Feed.new("Test Blog", "https://example.com", "My Testing Blog")
+	x.posts=postss
+	return x
 }
 
 pub fn (f Feed) rss() string {
@@ -58,5 +92,14 @@ pub fn (f Feed) rss() string {
 	out+="
 </channel>
 </rss>
-""
+"
+	return out
+}
+
+pub fn (f Feed) json() string {
+	return json.encode_pretty (f)
+}
+
+pub fn feed(s string) Feed {
+	return json.decode(Feed, s) or { panic(err) }
 }
--- src/main.v
+++ src/main.v
@@ -1,5 +1,10 @@
 module main
 
+import jsonfeed
+
 fn main() {
-	println('Hello World!')
+	mut myfeed:=jsonfeed.Feed.new("Test Blog", "https://example.com", "My Testing Blog")
+	myfeed.push(jsonfeed.Post.new("Test Post", "werdl", "A Testing post", "example.com/test"))
+
+	println(jsonfeed.Feed.rss(myfeed.rss()))
 }
